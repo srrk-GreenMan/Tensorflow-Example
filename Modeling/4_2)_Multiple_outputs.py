7af83a0eb6290752195788590d71b124d844a142
serereuk-tf <serereuk@kakao.com>
2021-06-07 01:34:43 +0900

    some fixes

--- Modeling/4_2)_Multiple_outputs.py
+++ Modeling/4_2)_Multiple_outputs.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import Model, Sequential, Input
+from tensorflow.keras.layers import Flatten, Dense, BatchNormalization
 
 
 # Multiple outputs
@@ -10,4 +11,12 @@ mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
+# 모델을 만들어보겠습니다.
+
+def build_AE():
+    inputs = Input((28, 28, 1))
+    x = Flatten()(inputs)
+    x = Dense(256, activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = Dense(64, activation='relu')(x)
 
